61babb1edf635e387ac145eae4440fbb8d46d669
IgorPidoprygora <igorpiddev@gmail.com>
2024-04-09 11:00:21 +0300

    move shapes

---
+++ components/layout/hero/Shapes.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="absolute top-[-200px] left-[-200px] w-[400px] h-[400px] bg-b-red-100 z-10 rounded-full" />
+  <div class="top-0 absolute left-0 m-auto w-[300px] h-[300px] bg-b-red-300 blur-[200px]" />
+  <div class="absolute bottom-0 right-0 m-auto w-[300px] h-[300px] bg-b-blue-dark-300 blur-[200px]" />
+  <div class="absolute bottom-[-200px] right-[-200px] w-[400px] h-[400px] bg-b-blue-dark-100 z-10 rounded-full" />
+</template>
