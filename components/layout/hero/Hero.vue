b0c9e2648cbabe46b6039d7c2895ddc752f47e40
IgorPidoprygora <igorpiddev@gmail.com>
2024-04-09 11:06:29 +0300

    hero modify

--- components/layout/hero/Hero.vue
+++ components/layout/hero/Hero.vue
@@ -5,12 +5,15 @@
       <h1 class="mb-6 text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-tight text-gray-900">
         Unleash Your Potential with Cloud Solutions
       </h1>
-      <h2 class="text-gray-600 text-lg lg:text-2xl mb-6">
+      <h2 class="text-gray-600 text-lg lg:text-2xl mb-10">
         Enjoy Peace of Mind with Our Secure and Reliable Cloud Storage Solutions
       </h2>
-      <div>
-        <nuxt-link to="/register" class="bg-gradient-to-r from-b-blue-dark-500 to-b-blue-dark-700 hover:bg-gradient-to-br text-white font-bold py-4 px-4 rounded-md">
+      <div class="flex gap-4 justify-center">
+        <nuxt-link to="/register" class="bg-gradient-to-r from-b-purple-500 to-b-blue-dark-700 hover:bg-gradient-to-br text-white font-bold py-4 px-4 rounded-md">
           Create free account
+        </nuxt-link>
+        <nuxt-link to="/register" class="border-solid border-2 border-b-blue-dark-700 text-b-blue-dark-700 hover:text-white hover:bg-b-blue-dark-700 font-bold py-4 px-4 rounded-md">
+          Show subscribe plans
         </nuxt-link>
       </div>
       <!-- <div class="flex flex-col w-1/2 p-4">
