7fca0c09007b16daafeccd39ee69207e3d8443ae
igorpiddev <igorpiddev@gmail.com>
2024-04-15 08:36:19 +0300

    menu

--- components/layout/navbar/Navbar.vue
+++ components/layout/navbar/Navbar.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <div class="flex items-center gap-[5px]">
-      <Icon name="ri:upload-cloud-2-line" />
-      <h2 class="uppercase font-bold">
+      <Icon name="ri:upload-cloud-2-line" size="30" />
+      <h1 class="uppercase font-bold text-xl">
         Skybox
-      </h2>
+      </h1>
     </div>
+    <nav>
+      <li v-for="(menuItemElement, menuItemIndex) in menuitems" :key="menuItemIndex">
+        <a :href="menuItemElement.title">{{ menuItemElement.link }}</a>
+      </li>
+    </nav>
     <div class="flex items-center gap-5">
-      <div>Log in</div>
+      <div>Sign in</div>
       <div>
         <button class="btn btn-primary__outline">
-          Sign in
+          Sign up
         </button>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { menuItem } from '~/types/menuItem'
+
+const menuitems = ref<menuItem[]>([
+  {
+    title: 'About',
+    link: '/about'
+  }
+])
 </script>
