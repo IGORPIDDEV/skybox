58e8d0ec6edf49fc929569a7bba4a619052710b6
igorpiddev <igorpiddev@gmail.com>
2024-04-16 10:00:16 +0300

    add styles

--- components/layout/header/Header.vue
+++ components/layout/header/Header.vue
@@ -1,7 +1,9 @@
 <template>
-  <header class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <LayoutHeaderLogo />
-    <LayoutHeaderNavbar />
-    <LayoutHeaderAuthMenu />
+  <header class="relative shadow-sm">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <LayoutHeaderLogo />
+      <LayoutHeaderNavbar />
+      <LayoutHeaderAuthMenu />
+    </div>
   </header>
 </template>
